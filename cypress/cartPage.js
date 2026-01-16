3256c9825d05cf9504425d1a2604968842cc315b
durjoysqa-eng <durjoysqa@gmail.com>
2026-01-16 18:25:21 +0600

    done

--- cypress/cartPage.js
+++ cypress/cartPage.js
@@ -2,42 +2,56 @@ class CartPage {
   elements = {
     addToCartBtn: () => cy.get('[data-test="add-to-cart"]'),
     quantityInput: () => cy.get('[data-test="product-quantity"]'),
-    linePrice: () => cy.get('[data-test="line-price"]'),
-    cartIcon: () => cy.get('svg[data-icon="cart-shopping"]')
+    linePrice: () => cy.get('span[data-test="line-price"]'),
+    cartIcon: () => cy.get('a[data-test="nav-cart"]') // cart link
   };
 
+  /**
+   * Add product to cart and navigate to Cart/Checkout page
+   */
   addProductToCart() {
+    // Scroll and click Add to Cart
     this.elements.addToCartBtn()
       .scrollIntoView({ duration: 500 })
       .should('be.visible')
       .click();
 
-    cy.wait(1000);
-
+    // Click cart icon to go to checkout/cart page
     this.elements.cartIcon()
       .scrollIntoView({ duration: 500 })
       .should('be.visible')
       .click();
 
-    // wait for line price to appear
-    cy.get('[data-test="line-price"]', { timeout: 10000 }).should('be.visible');
+    // Wait for line price to appear on cart page
+    this.elements.linePrice()
+      .scrollIntoView({ duration: 500 })
+      .should('be.visible');
   }
 
+  /**
+   * Change quantity of product in cart
+   */
   changeQuantity(quantity) {
     this.elements.quantityInput()
       .scrollIntoView({ duration: 500 })
       .clear()
       .type(quantity)
-      .blur();
+      .blur(); // trigger Angular update
 
-    // wait for price update
-    cy.get('[data-test="line-price"]', { timeout: 10000 }).should('be.visible');
+    // Wait for updated price to appear
+    this.elements.linePrice()
+      .scrollIntoView({ duration: 500 })
+      .should('be.visible');
   }
 
+  /**
+   * Get the current line price
+   */
   getPrice() {
-    return cy.get('[data-test="line-price"]', { timeout: 10000 })
-             .should('be.visible')
-             .invoke('text');
+    return this.elements.linePrice()
+      .scrollIntoView({ duration: 500 })
+      .should('be.visible')
+      .invoke('text');
   }
 }
 
--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,25 +1,29 @@
-import CartPage from '../cartPage'; // adjust the path to your file
+import CartPage from '../cartPage';
 
 describe('Add to Cart and Update Quantity', () => {
 
   it('Should update price when quantity changes', () => {
     cy.viewport(1920, 1080);
-    cy.visit('/product/01KF37CWV12ES8C9CSDRSZRKNS');
 
-    // ✅ Add product to cart and navigate to cart
+    // Visit product page
+    cy.visit('/product/01KF3ATVWGXZ85GSAXVN70P71K');
+
+    // Add product to cart and open cart page
     CartPage.addProductToCart();
-    cy.wait(20000)
 
-    // Capture initial price
+    // Capture initial line price
     CartPage.getPrice().then((initialPrice) => {
       const initialAmt = parseFloat(initialPrice.replace('$', ''));
 
       // Change quantity to 3
       CartPage.changeQuantity(3);
 
-      // Verify updated price is greater than initial
+      // Capture updated price
       CartPage.getPrice().then((updatedPrice) => {
         const updatedAmt = parseFloat(updatedPrice.replace('$', ''));
+        cy.log(`Initial: ${initialAmt}, Updated: ${updatedAmt}`);
+
+        // Verify updated price increased
         expect(updatedAmt).to.be.greaterThan(initialAmt * 2);
       });
     });
